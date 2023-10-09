7ec0343f6d563b5b0cd3096b798881845449af79
clarspau <125636195+clarspau@users.noreply.github.com>
2023-10-08 20:43:34 -0700

    Changed the answer on the 2nd query exercise. Added the 'HAVING' clause to filter the results and ensures that only owners with at least 1 vehicle are included in the result.

--- queries.sql
+++ queries.sql
@@ -17,6 +17,7 @@ FROM owners AS owners_table
 LEFT JOIN vehicles AS vehicles_table
 ON owners_table.id = vehicles_table.owner_id
 GROUP BY owners_table.first_name, owners_table.last_name
+HAVING COUNT(vehicles_table.owner_id) >= 1
 ORDER BY owners_table.first_name;
 
 -- Count the number of cars for each owner and display the average price for each of the cars as integers. Display the owners first_name , last_name, average price and count of vehicles. The first_name should be ordered in descending order. Only display results with more than one vehicle and an average price greater than 10000. 
