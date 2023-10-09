03fa779b21da915bcf05743008333094312b905c
clarspau <125636195+clarspau@users.noreply.github.com>
2023-10-08 20:55:57 -0700

    Answered the 3rd query exercise. Listed owners with >1 cars, avg. price > 10,000, sorted by first name (desc).

--- queries.sql
+++ queries.sql
@@ -12,7 +12,7 @@ ON owners_table.id = vehicles_table.owner_id;
 -- Count the number of cars for each owner. Display the owners 
 -- first_name , last_name and count of vehicles. The first_name should be ordered in ascending order.
 
-SELECT owners_table.first_name, owners_table.last_name, COUNT(vehicles_table.owner_id) AS vehicle_count
+SELECT owners_table.first_name, owners_table.last_name, COUNT(vehicles_table.owner_id) AS count
 FROM owners AS owners_table
 LEFT JOIN vehicles AS vehicles_table
 ON owners_table.id = vehicles_table.owner_id
@@ -20,4 +20,16 @@ GROUP BY owners_table.first_name, owners_table.last_name
 HAVING COUNT(vehicles_table.owner_id) >= 1
 ORDER BY owners_table.first_name;
 
+
 -- Count the number of cars for each owner and display the average price for each of the cars as integers. Display the owners first_name , last_name, average price and count of vehicles. The first_name should be ordered in descending order. Only display results with more than one vehicle and an average price greater than 10000. 
+
+SELECT first_name, last_name, 
+ROUND(AVG(price)) AS average_price, 
+COUNT(vehicles_table.owner_id)
+FROM owners AS owners_table
+LEFT JOIN vehicles AS vehicles_table
+ON owners_table.id = vehicles_table.owner_id
+GROUP BY (first_name, last_name)
+HAVING COUNT(vehicles_table.owner_id) > 1
+AND ROUND(AVG(price)) > 10000
+ORDER BY first_name DESC;
