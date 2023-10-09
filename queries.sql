d1a8e2ebc913c8427767df3683b90ba3a6a0514c
clarspau <125636195+clarspau@users.noreply.github.com>
2023-10-08 19:57:04 -0700

    initial commit with exercise questions

---
+++ queries.sql
@@ -0,0 +1,12 @@
+-- write your queries here
+
+-- Write the following SQL commands to produce the necessary output.
+-- Join the two tables so that every column and record appears, regardless of if there is not an owner_id .
+
+
+
+-- Count the number of cars for each owner. Display the owners 
+-- first_name , last_name and count of vehicles. The first_name should be ordered in ascending order.
+
+
+-- Count the number of cars for each owner and display the average price for each of the cars as integers. Display the owners first_name , last_name, average price and count of vehicles. The first_name should be ordered in descending order. Only display results with more than one vehicle and an average price greater than 10000. 
